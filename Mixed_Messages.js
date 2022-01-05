0e6b9749d7b3d7b490d5aabcb53493330328e68a
StuartABarnes <stuartab@hotmail.co.uk>
2022-01-05 16:38:21 +0000

    Added 3 word variables and maths

--- Mixed_Messages.js
+++ Mixed_Messages.js
@@ -1,1 +1,11 @@
 //This program will provide a series of randomly created messages via JavaScript
+const firstWords = [];
+const secondWords = [];
+const thirdWords = [];
+
+function mixedMessages (firstWords,secondWords,thirdWords) {
+    let randNumber1 = Math.floor(Math.random() * (firstWords.length));
+    let randNumber2 = Math.floor(Math.random() * (secondWords.length));
+    let randNumber3 = Math.floor(Math.random() * (thirdWords.length));
+    
+}
